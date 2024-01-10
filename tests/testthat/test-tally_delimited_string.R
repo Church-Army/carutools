6f7d2ebb62e6341632ff1f2f3a51ea1daed29c0c
dave-lovell <davidpg.lovell@gmail.com>
2024-01-10 15:08:17 +0000

    testing tally_delimited_names fails rightly when names duplicated

--- tests/testthat/test-tally_delimited_string.R
+++ tests/testthat/test-tally_delimited_string.R
@@ -16,3 +16,24 @@ test_that("tally-delimited-string works", {
   expect_length(result, 5)
 
   })
+
+test_that("Duplicate names are handled properly", {
+
+  funny_names <- data.frame(
+    people = c("anna", "bob", "chloe", "dave"),
+    fruit  = c("banana, apple, apple, pear, BANANA",
+               "BANANA, pear",
+               "",
+               "apple, peach")
+  )
+
+  result <- tally_delimited_string(funny_names, fruit)
+
+  expect_length(result, 5)
+
+  expect_error(
+    tally_delimited_string(funny_names, fruit, names_repair = stringr::str_to_lower),
+    class = "duplicate_names"
+    )
+
+})
